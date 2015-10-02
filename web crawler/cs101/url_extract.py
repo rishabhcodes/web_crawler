6fbe2a1bfc5678b109dfcbb63719604fc53a8f3f
risshah <interests.rishabh@gmail.com>
2015-10-02 20:25:53 +0530

    Extracts all links from a web page

--- web crawler/cs101/url_extract.py
+++ web crawler/cs101/url_extract.py
@@ -5,10 +5,15 @@ Created on Sep 27, 2015
 '''
 # temporarily using page directly. Will later be passed as parameter to this function
 
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://www.xkcd.com">')
-
-#Extracting links can be a function, hence creating a procedure for the same
+#Utility function to get web page as a string
+def get_page(url):
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ''
+    
+#Used as a utility function to extract one link at a time from the web page
 def get_next_target(page):
 
     # pattern to be isolated
@@ -17,15 +22,38 @@ def get_next_target(page):
     # increment is used move the pointer in string appropriately
     increment = 1
     
-    # starting position of url  
-    start_link = (page.find(pattern)) + len(pattern) + increment
+    if page.find(pattern) != -1:
     
-    # ending position of url
-    end_link = page.find('"',start_link)
-    
-    # isolated url just being printed for now
-    url = page[start_link:end_link]
-    
-    return(url, end_link)
+        # starting position of url  
+        start_link = (page.find(pattern)) + len(pattern) + increment
+        
+        # ending position of url
+        end_link = page.find('"',start_link)
+        
+        # isolated url just being printed for now
+        url = page[start_link:end_link]
+        
+        return(url, end_link)
+    return(None, 0)
 
-print get_next_target(page)
+#Extracts all links from a web page 
+def print_all_links(page):
+    while True:
+        url,end_link = get_next_target(page)
+        if not url:
+            break
+        else:
+            print url
+            page = page[end_link:]
+    return    
+
+page = get_page('http://xkcd.com/353/')        
+print_all_links(page)
+        
+        
+        
+        
+        
+        
+        
+        
