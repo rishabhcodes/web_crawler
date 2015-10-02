b98b6a802397d43e4bf38ea6a3aa7dcbbbcb21af
risshah <interests.rishabh@gmail.com>
2015-10-02 13:57:26 +0530

    Build a procedure for extracting links

--- web crawler/cs101/url_extract.py
+++ web crawler/cs101/url_extract.py
@@ -8,21 +8,24 @@ Created on Sep 27, 2015
 page =('<div id="top_bin"><div id="top_content" class="width960">'
 '<div class="udacity float-left"><a href="http://www.xkcd.com">')
 
-# pattern to be isolated
+#Extracting links can be a function, hence creating a procedure for the same
+def get_next_target(page):
 
-pattern = '<a href='
+    # pattern to be isolated
+    pattern = '<a href='
+    
+    # increment is used move the pointer in string appropriately
+    increment = 1
+    
+    # starting position of url  
+    start_link = (page.find(pattern)) + len(pattern) + increment
+    
+    # ending position of url
+    end_link = page.find('"',start_link)
+    
+    # isolated url just being printed for now
+    url = page[start_link:end_link]
+    
+    return(url, end_link)
 
-# increment is used move the pointer in string appropriately
-increment = 1
-
-# initialized url
-url = ''
- 
-# starting position of url  
-start_link = (page.find(pattern)) + len(pattern) + increment
-
-# ending position of url
-end_link = page.find('"',start_link)
-
-# isolated url just being printed for now
-print page[start_link:end_link]
+print get_next_target(page)
