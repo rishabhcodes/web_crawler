03819fe9195e5be63020a1cf7d9aa61e2f07c177
risshah <interests.rishabh@gmail.com>
2015-10-04 21:44:17 +0530

    Improves design of index through hash tables for better performance

--- web crawler/cs101/url_extract.py
+++ web crawler/cs101/url_extract.py
@@ -24,7 +24,30 @@ def union(l1, l2):
         if e not in l1:
             l1.append(e)
     return
-    
+
+def keyword_hash(keyword):
+    """takes a string and returns a hash value"""
+    hv = 0
+    for c in keyword:
+        hv = hv + ord(c)
+    return hv
+        
+def hash_bucket(keyword,nbuckets):
+    '''takes a keyword and total buckets & return the bucket specific to keyword'''
+    hv = keyword_hash(keyword)
+    bucket = hv%nbuckets
+    return bucket
+
+def create_index(nbuckets):
+    '''creates and initializes the web index'''
+    index = []
+    i=0
+    while i < nbuckets:
+        index.append([])
+        i=i+1
+    return index
+
+  
 #Used as a utility function to extract one link at a time from the web page
 def get_next_target(page):
     '''this function identifies the next url given the 
@@ -63,37 +86,38 @@ def get_all_links(page):
             page = page[end_link:]
     return url_list   
 
-def add_to_index(index,keyword,url):
+def add_to_index(index,keyword,url,buckets):
     """This function takes a keyword & mutates the Index
     creating a mapping of the keyword to urls"""
-
-    for e in index:
+    bucket = hash_bucket(keyword, buckets)
+    for e in index[bucket]:
         if e[0] == keyword:
             e[1].append(url)
             return
-    index.append([keyword,[url]])
+    index[bucket].append([keyword,[url]])
     return
 
-def add_page_to_index(index,page,url):
+def add_page_to_index(index,page,url,buckets):
     '''adds the crawled web pages to the index'''
     for e in page.split():
-        add_to_index(index,e,url)
+        add_to_index(index,e,url,buckets)
     return
 
 def crawler(seed):
     '''starts the crawl process provided a seed page'''
     to_crawl = [seed]
     crawled = []
-    index = []
+    buckets = 4
+    index = create_index(buckets)
     while to_crawl:
         url = to_crawl.pop()
         if url not in crawled:
             page = get_page(url)
-            add_page_to_index(index, page, url)
+            add_page_to_index(index, page, url, buckets)
             union(to_crawl,get_all_links(page))
             crawled.append(url)
         #need to figure out how to remove e from to_crawl
-    return index, crawled  
+    return index 
 
 
 print crawler('http://www.udacity.com/cs101x/index.html')        
