24ecf17739400ff56eacdeebfe46a278b32caa7e
risshah <interests.rishabh@gmail.com>
2015-10-04 08:17:07 +0530

    Crawls the web with a seed page & returns an index

--- web crawler/cs101/url_extract.py
+++ web crawler/cs101/url_extract.py
@@ -7,14 +7,28 @@ Created on Sep 27, 2015
 
 #Utility function to get web page as a string
 def get_page(url):
+    '''this function returns web page as a string
+    given url as input'''
     try:
         import urllib
         return urllib.urlopen(url).read()
     except:
         return ''
     
+
+def union(l1, l2):
+    """takes 2 lists l1 and l2 as input and returns
+    set union of the two"""
+    
+    for e in l2:
+        if e not in l1:
+            l1.append(e)
+    return
+    
 #Used as a utility function to extract one link at a time from the web page
 def get_next_target(page):
+    '''this function identifies the next url given the 
+    web page as a string'''
 
     # pattern to be isolated
     pattern = '<a href='
@@ -37,18 +51,53 @@ def get_next_target(page):
     return(None, 0)
 
 #Extracts all links from a web page 
-def print_all_links(page):
+def get_all_links(page):
+    '''this function extracts all links from a web page'''
+    url_list = []
     while True:
         url,end_link = get_next_target(page)
         if not url:
             break
         else:
-            print url
+            url_list.append(url)
             page = page[end_link:]
-    return    
+    return url_list   
 
-page = get_page('http://xkcd.com/353/')        
-print_all_links(page)
+def add_to_index(index,keyword,url):
+    """This function takes a keyword & mutates the Index
+    creating a mapping of the keyword to urls"""
+
+    for e in index:
+        if e[0] == keyword:
+            e[1].append(url)
+            return
+    index.append([keyword,[url]])
+    return
+
+def add_page_to_index(index,page,url):
+    '''adds the crawled web pages to the index'''
+    for e in page.split():
+        add_to_index(index,e,url)
+    return
+
+def crawler(seed):
+    '''starts the crawl process provided a seed page'''
+    to_crawl = [seed]
+    crawled = []
+    index = []
+    while to_crawl:
+        url = to_crawl.pop()
+        if url not in crawled:
+            page = get_page(url)
+            add_page_to_index(index, page, url)
+            union(to_crawl,get_all_links(page))
+            crawled.append(url)
+        #need to figure out how to remove e from to_crawl
+    return index, crawled  
+
+
+print crawler('http://www.udacity.com/cs101x/index.html')        
+
         
         
         
